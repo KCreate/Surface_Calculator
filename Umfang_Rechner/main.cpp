0940b121f04bd248420f5ccf7bf58a11d72161fe
Leonard Schuetz <leonardschuetz@Leonards-MacBook-Pro.local>
2013-12-25 03:27:50 +0100

    Initial Commit

---
+++ Umfang_Rechner/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Umfang_Rechner
+//
+//  Created by Leonard Schuetz on 25.12.13.
+//  Copyright (c) 2013 Leonard Schuetz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
